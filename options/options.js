974f4dab616f132567ed128f10618ad0aa27b8f8
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-04-23 21:14:48 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "datamodel-dev-78501",
-  url: "https://datamodel-dev-78501.botics.co",
-  api: "https://datamodel-dev-78501.botics.co/api/v1"
-}
+  "name": "datamodel-dev-78501",
+  "url": "https://datamodel-dev-78501.botics.co",
+  "api": "https://datamodel-dev-78501.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
